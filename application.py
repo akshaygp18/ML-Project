8910947b25714b500305b67df63bd001ba814296
akshaygp18 <akshaygp41@gmail.com>
2023-03-29 17:39:34 +0530

    Updated Application File

--- application.py
+++ application.py
@@ -13,7 +13,7 @@ app=application
 
 @app.route('/')
 def index():
-    return render_template(index.html)
+    return render_template('index.html')
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
@@ -42,5 +42,5 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8080)        
+    app.run(host="0.0.0.0")        
 
